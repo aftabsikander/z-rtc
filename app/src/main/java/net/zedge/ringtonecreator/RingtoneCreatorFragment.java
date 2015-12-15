6b9f414e229161a682552706ac27d38bd9778369
Nir <nir@zedge.net>
2015-12-15 23:40:58 +0200

    change encoder to AAC

--- app/src/main/java/net/zedge/ringtonecreator/RingtoneCreatorFragment.java
+++ app/src/main/java/net/zedge/ringtonecreator/RingtoneCreatorFragment.java
@@ -79,9 +79,9 @@ public class RingtoneCreatorFragment extends Fragment {
 
         mRecorder = new MediaRecorder();
         mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
         mRecorder.setOutputFile(mFileName);
-        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
 
         try {
             mRecorder.prepare();
