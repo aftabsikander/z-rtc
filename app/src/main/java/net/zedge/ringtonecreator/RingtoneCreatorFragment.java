fce57216705d5167852c9ba4cbc9d695bfe56894
Nir <nir@zedge.net>
2015-12-16 10:17:28 +0200

    reset counter/visual when stop button pushed

--- app/src/main/java/net/zedge/ringtonecreator/RingtoneCreatorFragment.java
+++ app/src/main/java/net/zedge/ringtonecreator/RingtoneCreatorFragment.java
@@ -28,13 +28,8 @@ public class RingtoneCreatorFragment extends Fragment {
     private static final String ARG_SECTION_NUMBER = "section_number";
     private static final String LOG = "RingtoneCreatorFragment";
 
-
     private static String mFileName = null;
-
-//    private RecordButton mRecordButton = null;
     private MediaRecorder mRecorder = null;
-
-//    private PlayButton   mPlayButton = null;
     private MediaPlayer mPlayer = null;
 
     Handler handler;
@@ -159,11 +154,6 @@ public class RingtoneCreatorFragment extends Fragment {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                c=0;
-                mVisualizerView.clear();
-                mVisualizerView.invalidate(); // refresh the VisualizerView
-
-                mTextCounter.setText(getTimeAsText());
 
             }
         });
@@ -177,11 +167,6 @@ public class RingtoneCreatorFragment extends Fragment {
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                c=0;
-                mVisualizerView.clear();
-                mVisualizerView.invalidate(); // refresh the VisualizerView
-
-                mTextCounter.setText(getTimeAsText());
 
             }
         });
@@ -197,6 +182,13 @@ public class RingtoneCreatorFragment extends Fragment {
                 } else {
                     textView.setText("Start recording");
                     imageView.setImageResource(R.drawable.rec_btn);
+
+                    c=0;
+                    mVisualizerView.clear();
+                    mVisualizerView.invalidate(); // refresh the VisualizerView
+
+                    mTextCounter.setText(getTimeAsText());
+
                 }
 
                 mStartRecording = !mStartRecording;
@@ -204,64 +196,10 @@ public class RingtoneCreatorFragment extends Fragment {
             }
         });
 
-//        mRecordButton = new RecordButton(getContext());
-//        ll.addView(mRecordButton,
-//                new LinearLayout.LayoutParams(
-//                        ViewGroup.LayoutParams.WRAP_CONTENT,
-//                        ViewGroup.LayoutParams.WRAP_CONTENT,
-//                        0));
-//        mPlayButton = new PlayButton(getContext());
-//        ll.addView(mPlayButton,
-//                new LinearLayout.LayoutParams(
-//                        ViewGroup.LayoutParams.WRAP_CONTENT,
-//                        ViewGroup.LayoutParams.WRAP_CONTENT,
-//                        0));
         return rootView;
     }
 
-//    class RecordButton extends Button {
-//        boolean mStartRecording = true;
-//
-//        OnClickListener clicker = new OnClickListener() {
-//            public void onClick(View v) {
-//                onRecord(mStartRecording);
-//                if (mStartRecording) {
-//                    setText("Stop recording");
-//                } else {
-//                    setText("Start recording");
-//                }
-//                mStartRecording = !mStartRecording;
-//            }
-//        };
-//
-//        public RecordButton(Context ctx) {
-//            super(ctx);
-//            setText("Start recording");
-//            setOnClickListener(clicker);
-//        }
-//    }
-
-//    class PlayButton extends Button {
-//        boolean mStartPlaying = true;
-//
-//        OnClickListener clicker = new OnClickListener() {
-//            public void onClick(View v) {
-//                onPlay(mStartPlaying);
-//                if (mStartPlaying) {
-//                    setText("Stop playing");
-//                } else {
-//                    setText("Start playing");
-//                }
-//                mStartPlaying = !mStartPlaying;
-//            }
-//        };
-//
-//        public PlayButton(Context ctx) {
-//            super(ctx);
-//            setText("Start playing");
-//            setOnClickListener(clicker);
-//        }
-//    }
+}
 
     @Override
     public void onPause() {
